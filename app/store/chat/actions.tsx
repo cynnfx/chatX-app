d9c8dfcd604429d023040aadc573294fb6c37ca8
Charles OUDIN <charles.oudin@hotmail.fr>
2020-11-19 17:42:39 +0100

    remove juan store

--- app/store/chat/actions.tsx
+++
@@ -1,9 +0,0 @@
-import { Message, SEND_MESSAGE, ChatActionTypes } from './types'
-
-// TypeScript infers that this function is returning SendMessageAction
-export function sendMessage(newMessage: Message): ChatActionTypes {
-  return {
-    type: SEND_MESSAGE,
-    payload: newMessage
-  }
-}
